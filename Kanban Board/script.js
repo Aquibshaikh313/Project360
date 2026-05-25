89c9416d9b6dc1ba717b1ed93f29102380686e6f
Aquib <aquib.shaikh256@gmail.com>
2026-05-26 02:03:40 +0530

    Updated my project,added few more features today

--- Kanban Board/script.js
+++ Kanban Board/script.js
@@ -39,15 +39,24 @@ removeBtn.addEventListener("click", function () {
   }
 });
 
+// Creating funtion to remove the newly created ticket
+function handleRemove(ticket){
+  ticket.addEventListener("click",function(){
+    if(removeTaskFlag){
+      ticket.remove();
+    }
+  })
+}
 
-function createTicket(){
+
+function createTicket(ticketColor,ticketID,ticketTask){
 
   const ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
   ticketCont.innerHTML = `
-  <div class="ticket-color"></div>
-        <div class="ticket-id">1234</div>
-        <div class="task-area">Random Task</div>
+  <div class="ticket-color" style="background-color:${ticketColor}"></div>
+        <div class="ticket-id">${ticketID}</div>
+        <div class="task-area">${ticketTask}</div>
         <div class="ticket-lock">
           <i class="fa-solid fa-lock"></i>
         </div>
@@ -56,13 +65,24 @@ function createTicket(){
   mainCont.appendChild(ticketCont);
   // console.log(ticketCont);
 
+  handleRemove(ticketCont);
+
 }
 
 modalCont.addEventListener("keydown", function(e){
   const keypress = e.key;
 
   if(keypress === "Enter"){
-    createTicket();
+
+    const ticketTask = textAreaCont.value.trim();
+    if(ticketTask === ""){
+      alert("Enter the task first")
+      return; // why return bcz if not return then it will not passs an empty ticketTask
+    }
+    const ticketColor = byDefaultSeleColor;
+    const ticketID = shortid.generate();
+    
+    createTicket(ticketColor,ticketID,ticketTask);
 
     modalCont.style.display = "none";
     
@@ -72,3 +92,44 @@ modalCont.addEventListener("keydown", function(e){
 
   }
 })
+
+
+allPriorityColors = document.querySelectorAll(".priority-color");
+
+const colors = ["lightpink","lightgreen","lightblue","black"];
+
+let byDefaultSeleColor = colors[colors.length - 1];
+
+// 1) grabs all the colors elem from html
+    allPriorityColors.forEach(function(color){
+  // 2) listen for a click on every individual
+  color.addEventListener("click",function(){
+    
+  // 3) the moment we click on any color the active class gets wipe off
+   allPriorityColors.forEach(function(selectedColor){
+    selectedColor.classList.remove("active");
+
+  });
+   
+   //attaching the active class to seleted colr
+   color.classList.add("active");
+
+   byDefaultSeleColor = color.classList[0];
+  })
+
+})
+
+// alternate way to do the above thing
+// allPriorityColors.forEach(function(color) {
+//   color.addEventListener("click", function() {
+    
+//     // Find the SINGLE element that currently has 'active' and remove it
+//     const currentActive = document.querySelector(".priority-color.active");
+//     if (currentActive) {
+//       currentActive.classList.remove("active");
+//     }
+
+//     // Add 'active' to the newly clicked element
+//     color.classList.add("active");
+//   });
+// });
